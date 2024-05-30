27ab11da85de46be483283430e682d78011dc2f1
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-05-30 10:52:09 +0530

    One to Few Implementation

--- Chapter 4/RELATIONSHIPS/Models/user.js
+++ Chapter 4/RELATIONSHIPS/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose= require("mongoose");
+const { Schema }= mongoose;
 
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
@@ -6,3 +7,34 @@ async function main(){
 
 main().then(()=>console.log("Connected to Mongoose"))
 .catch((err)=>console.log(err));
+
+const userSchema= new Schema({
+    username: String,
+    address: [
+        {
+            // _id: false,
+            location: String,
+            city: String,
+        }
+    ],
+});
+
+const User= mongoose.model("User", userSchema);
+
+const addUser= async()=>{
+    const user1= new User({
+        username: "Rahul",
+        address: [
+            {
+                location: "33, Shiv Colony",
+                city: "Kota",
+            }
+        ],
+    });
+    user1.address.push({location: "76, Laxmi Nagal", city: "Kota"});
+    let result= await user1.save();
+    console.log(result);
+}
+
+addUser();
+
